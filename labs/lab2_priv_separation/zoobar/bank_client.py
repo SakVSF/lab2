16640cf208acfa78310e8aac4013f05de88d571b
Saakshi Vinod Saraf <76583844+SakVSF@users.noreply.github.com>
2024-04-10 12:30:56 +0800

    Update bank_client.py

--- labs/lab2_priv_separation/zoobar/bank_client.py
+++ labs/lab2_priv_separation/zoobar/bank_client.py
@@ -2,24 +2,25 @@ from debug import *
 from zoodb import *
 import rpclib
 
-def transfer(sender, recipient, zoobars):
+
+
+
+def transfer(sender, recipient, zoobars, token):
     ## Fill in code here.
-    c = rpclib.client_connect("/banksvc/sock")
-    return c.call('transfer', sender=sender, recipient=recipient, zoobars=zoobars)
+    with rpclib.client_connect('/banksvc/sock') as c:
+        return c.call('transfer', sender=sender, recipient=recipient, zoobars=zoobars, token=token)
 
 def balance(username):
     ## Fill in code here.
-    c = rpclib.client_connect("/banksvc/sock")
-    return c.call('balance', username=username)
-
+    with rpclib.client_connect('/banksvc/sock') as c:
+        return c.call('balance', username=username)
 
 def get_log(username):
     ## Fill in code here.
-    c = rpclib.client_connect("/banksvc/sock")
-    return c.call('get_log' , username=username)
+    with rpclib.client_connect('/banksvc/sock') as c:
+        return c.call('get_log', username=username)
 
-def account_creation(username):
-    c = rpclib.client_connect("/banksvc/sock")
-    return c.call('account_creation', username=username)
-
-
+def initalise_zoobars(username):
+    ## Fill in code here.
+    with rpclib.client_connect('/banksvc/sock') as c:
+        return c.call('initalise_zoobars', username=username)
